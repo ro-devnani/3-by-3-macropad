1d2a2c1d70103f375d3dc28bc191eaf755040d05
ro-devnani <143772494+ro-devnani@users.noreply.github.com>
2025-11-07 23:31:48 -0800

    Create keymap.c

---
+++ firmware/keymaps/default/keymap.c
@@ -0,0 +1,27 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ A │ B │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * ├───┼───┼───┤
+     * │ G │ H │ I │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_3x3(
+        KC_A,    KC_B,    KC_C,  
+        KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][SUM_DIRECTIONS] ={
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif
